876181a0398e2e0ff50f05df02e7b1ff6f5869c7
momaek <momaek17@gmail.com>
2021-04-16 18:50:47 +0800

    finish

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/momaek/formattag
 
 go 1.16
+
+require github.com/fatih/structtag v1.2.0 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,252 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"go/ast"
+	"go/format"
+	"go/parser"
+	"go/token"
+	"io/ioutil"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/fatih/structtag"
+)
+
+// TestStruct this is a test struct
+type TestStruct struct {
+	ID            string `json:"id"              xml:"id"`
+	IfNotModified string `json:"if_not_modified" xml:"if_not_modified"`
+	Name          string `json:"name"            xml:"name"`
+
+	ThisIsAStructWodeTianNa struct {
+		FieldFromThisIsAStructWodeTianNa string `json:"field_from_this_is_a_struct_wode_tian_na" xml:"field_from_this_is_a_struct_wode_tian_na"`
+		TianName                         string `json:"tian_name"                                xml:"tian_name"`
+	} `json:"this_is_a_struct_wode_tian_na" xml:"this_is_a_struct_wode_tian_na"`
+
+	T    time.Time `json:"t"    xml:"t"`
+	Fset Fset      `json:"fset" xml:"fset"`
+}
+
+type Fset struct{}
+
+type config struct {
+	fset   *token.FileSet
+	file   string
+	groups []group
+}
 
 func main() {
-	fmt.Println("vim-go")
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	c, err := parseConfig(os.Args[1:])
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	node, err := c.parse()
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	c.format(node)
+	err = c.write(node)
+	if err != nil {
+		log.Fatal("write file failed", err)
+	}
+
+}
+
+func (c *config) write(node ast.Node) error {
+	for _, grp := range c.groups {
+		for _, line := range grp.lines {
+			line.field.Tag.Value = line.result
+		}
+	}
+
+	var buf bytes.Buffer
+	err := format.Node(&buf, c.fset, node)
+	if err != nil {
+		return err
+	}
+
+	err = ioutil.WriteFile(c.file, buf.Bytes(), 0)
+	return err
+}
+
+func parseConfig(args []string) (*config, error) {
+	var (
+		file = flag.String("file", "", "Filename to be format")
+	)
+
+	if err := flag.CommandLine.Parse(args); err != nil {
+		return nil, err
+	}
+
+	if flag.NFlag() == 0 {
+		fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
+		flag.PrintDefaults()
+		return nil, flag.ErrHelp
+	}
+
+	c := &config{
+		file:   *file,
+		groups: []group{},
+	}
+
+	return c, nil
+}
+
+func (c *config) parse() (ast.Node, error) {
+	c.fset = token.NewFileSet()
+	var content interface{}
+	return parser.ParseFile(c.fset, c.file, content, parser.ParseComments)
+}
+
+func (c *config) format(node ast.Node) (ast.Node, error) {
+
+	ast.Inspect(node, c.rewrite)
+	c.process()
+	return nil, nil
+}
+
+type line struct {
+	field     *ast.Field
+	tags      []string
+	lens      []int
+	spaceLens []int
+	result    string
+}
+
+func (c *config) rewrite(node ast.Node) bool {
+	st, ok := node.(*ast.StructType)
+	if !ok {
+		return true
+	}
+
+	if len(st.Fields.List) == 0 {
+		return true
+	}
+
+	c.preProcessStruct(st)
+
+	return true
+}
+
+type group struct {
+	maxTagNum int
+	lines     []*line
+}
+
+func (c *config) preProcessStruct(st *ast.StructType, inline ...bool) {
+	lastLineNum := c.fset.Position(st.Fields.List[0].Pos()).Line
+	grp := group{}
+	l := len(st.Fields.List)
+	for idx, field := range st.Fields.List {
+		if field.Tag == nil {
+			continue
+		}
+
+		tag, err := strconv.Unquote(field.Tag.Value)
+		if err != nil {
+			continue
+		}
+
+		tag = strings.TrimLeft(tag, " ")
+		tag = strings.TrimRight(tag, " ")
+
+		tags, err := structtag.Parse(tag)
+		if err != nil {
+			log.Printf("parse tag failed %v, Field: %v, Tag: %s", err, field.Names, field.Tag.Value)
+			continue
+		}
+
+		if _, ok := field.Type.(*ast.StructType); ok {
+			if idx+1 < l {
+				lastLineNum = c.fset.Position(st.Fields.List[idx+1].Pos()).Line
+			}
+
+			grp = group{}
+			continue
+		}
+
+		if grp.maxTagNum < tags.Len() {
+			grp.maxTagNum = tags.Len()
+		}
+
+		ln := &line{
+			field: field,
+		}
+
+		lens := make([]int, 0, tags.Len())
+		for _, key := range tags.Keys() {
+			t, _ := tags.Get(key)
+			lens = append(lens, len(t.String()))
+			ln.tags = append(ln.tags, t.String())
+		}
+
+		ln.lens = lens
+
+		lineNum := c.fset.Position(field.Pos()).Line
+		if lineNum-lastLineNum >= 2 {
+			lastLineNum = lineNum
+			c.groups = append(c.groups, grp)
+			grp = group{}
+			continue
+		}
+
+		lastLineNum = lineNum
+		grp.lines = append(grp.lines, ln)
+	}
+
+	if len(grp.lines) > 0 {
+		c.groups = append(c.groups, grp)
+	}
+}
+
+func (c *config) process() {
+	for _, grp := range c.groups {
+		if len(grp.lines) == 1 {
+			continue
+		}
+
+		for i := 0; i < grp.maxTagNum; i++ {
+			max := process(grp.lines, i)
+			updateResult(grp.lines, max, i)
+		}
+
+		for _, line := range grp.lines {
+			line.result = "`" + line.result + "`"
+		}
+	}
+}
+
+func process(lines []*line, idx int) int {
+	max := 0
+	for _, line := range lines {
+		if len(line.lens) > idx {
+			if line.lens[idx] > max {
+				max = line.lens[idx]
+			}
+		}
+	}
+
+	return max
+}
+
+func updateResult(lines []*line, max, idx int) {
+	for _, line := range lines {
+		if l := len(line.lens); l > idx && idx < l-1 {
+			line.result += line.tags[idx] + strings.Repeat(" ", max-line.lens[idx]+1)
+		} else {
+			line.result += line.tags[idx]
+		}
+	}
 }
